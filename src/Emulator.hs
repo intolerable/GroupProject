2343d88c2bd05e6675483c13dd0f3e20ba32a23d
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-08 01:50:53 +0000

    basic gl window

--- src/Emulator.hs
+++ src/Emulator.hs
@@ -1,4 +1,24 @@
 module Emulator where
 
+import Graphics.Rendering.OpenGL
+import qualified Graphics.UI.GLUT as GLUT
+
 main :: IO ()
-main = putStrLn "Hello, emulator"
+main = do
+  initGL
+  GLUT.mainLoop
+
+initGL :: IO ()
+initGL = do
+  _ <- GLUT.getArgsAndInitialize
+  GLUT.initialDisplayMode $=
+    [ GLUT.RGBAMode
+    , GLUT.WithAlphaComponent
+    , GLUT.DoubleBuffered ]
+  _ <- GLUT.createWindow "GBA"
+  GLUT.windowSize $= Size 240 160
+  GLUT.viewport $= (Position 0 0, Size 240 160)
+  GLUT.clearColor $= Color4 0 0 0 0
+  GLUT.displayCallback $= do
+    clear [GLUT.ColorBuffer]
+    GLUT.swapBuffers
