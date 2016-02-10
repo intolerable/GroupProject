db3a3298b05e6030077c7d5a721039d66ebf9304
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-10 03:13:07 +0000

    implement default writeWord / readWord

--- src/Emulator/Memory.hs
+++ src/Emulator/Memory.hs
@@ -6,6 +6,7 @@ import Control.Monad.Trans.State.Strict
 import Data.Array.IO
 import Data.Array.MArray
 import Data.Array.Unboxed
+import Data.Bits
 import Prelude hiding (Word)
 
 type Memory = UArray Address Byte
@@ -16,10 +17,20 @@ class Mem m where
   writeByte :: Address -> Byte -> m ()
   readByte :: Address -> m Byte
 
+  -- assume little endian
   writeWord :: Address -> Word -> m ()
-  writeWord = undefined -- based on multiple writeBytes
+  writeWord addr word = do
+    writeByte addr (fromIntegral word)
+    writeByte (addr + 1) (fromIntegral word `shiftR` 8)
+    writeByte (addr + 2) (fromIntegral word `shiftR` 16)
+    writeByte (addr + 3) (fromIntegral word `shiftR` 24)
   readWord :: Address -> m Word
-  readWord = undefined -- based on multiple readBytes
+  readWord addr = do
+    b1 <- fromIntegral <$> readByte addr
+    b2 <- fromIntegral <$> readByte (addr + 1)
+    b3 <- fromIntegral <$> readByte (addr + 2)
+    b4 <- fromIntegral <$> readByte (addr + 3)
+    b1 .|. (b2 `shiftL` 8) .|. (b3 `shiftL` 16) .|. (b4 `shiftL` 24)
 
 newtype PureMemory a = PureMemory (State Memory a)
   deriving (Functor, Applicative, Monad)
