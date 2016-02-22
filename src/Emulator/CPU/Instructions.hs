417d7f69fd387655fd0370e9fdcdfed88349ff5b
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-22 15:35:22 +0000

    add an example instruction

--- src/Emulator/CPU/Instructions.hs
+++ src/Emulator/CPU/Instructions.hs
@@ -1,1 +1,15 @@
 module Emulator.CPU.Instructions where
+
+import Emulator.Types
+import Emulator.CPU
+
+import Control.Lens
+import Control.Monad.State.Class
+
+type RegisterLabel = Lens' Registers MWord
+
+add :: MonadState Registers m => RegisterLabel -> RegisterLabel -> RegisterLabel -> m ()
+add dest src1 src2 = do
+  res1 <- use src1
+  res2 <- use src2
+  dest .= res1 + res2
