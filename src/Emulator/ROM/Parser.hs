cf303411108d53b042429b8fa1e5956c0f603ad4
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-23 22:30:29 +0000

    use binary instead of binary-strict for better compatibility

--- src/Emulator/ROM/Parser.hs
+++ src/Emulator/ROM/Parser.hs
@@ -2,35 +2,29 @@ module Emulator.ROM.Parser (readROM) where
 
 import Emulator.ROM
 
-import Prelude hiding (readFile)
-import Data.ByteString
-import Data.Binary.Strict.Get
+import Control.Applicative
+import Data.Binary.Get
+import Data.ByteString.Lazy (ByteString)
 import Data.Word
-
+import Prelude
+import qualified Data.ByteString.Lazy as BS
 
 -- Given a filename, return the data and header of the ROM, or an error.
 readROM :: String -> IO (Either String (ROMHeader, ByteString))
 readROM path = do
-  contents <- readFile path
-  return $ parseROM contents
-
+  parseROM <$> BS.readFile path
 
 parseROM :: ByteString -> Either String (ROMHeader, ByteString)
 parseROM str =
-  case runGet parseHeader str of
-    (Right (branch, logo, title, gCode, maker, magic), remainder) ->
-      Right (ROMHeader branch logo title gCode maker magic, remainder)
-    ((Left err, _)) ->
-      Left $ "ROM parsing error: " ++ err
+  case runGetOrFail parseHeader str of
+    Left (_, _, err) -> Left err
+    Right (rest, _, rh) -> Right (rh, rest)
 
-parseHeader :: Get (ByteString, ByteString, ByteString, ByteString, ByteString, Word8)
-parseHeader = do
-  branch   <- getByteString 4   -- Branch instruction
-  logo     <- getByteString 156 -- Nintendo logo
-  title    <- getByteString 12  -- The game title
-  gameCode <- getByteString 4   -- The game's code
-  maker    <- getByteString 2   -- The maker code
-  magic    <- getWord8          -- The magic number
-  -- Skip past 13+36 reserved+multiboot bytes
-  skip 49
-  return (branch, logo, title, gameCode, maker, magic)
+parseHeader :: Get ROMHeader
+parseHeader =
+  ROMHeader <$> getByteString 4 -- branch instruction
+            <*> getByteString 156 -- Nintendo logo
+            <*> getByteString 12 -- game's title
+            <*> getByteString 4 -- game's code
+            <*> getByteString 2 -- maker's code
+            <*> (getWord8 <* skip (13 + 36)) -- magic byte, then skip reserved (13) and multiboot (36) bytes
