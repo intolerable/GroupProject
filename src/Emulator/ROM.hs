ae7f86a88cffc664fcdb2e25886531a0d6ea4b00
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-24 02:48:04 +0000

    fix lens generation

--- src/Emulator/ROM.hs
+++ src/Emulator/ROM.hs
@@ -6,12 +6,12 @@ import Control.Lens
 import Data.ByteString (ByteString)
 
 data ROMHeader = ROMHeader
-  { _romHeaderStartLoc :: ByteString -- 0x00: B <game_code_start>
-  , _romHeaderNintendoLogo :: ByteString  -- 0x04: nintendo logo
-  , _romHeaderGameTitle :: ByteString  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
-  , _romHeaderGameCode :: ByteString  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
-  , _romHeaderMakerCode :: ByteString  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
-  , _romHeaderMagicByte :: Byte }    -- 0xB2: Must be 0x96
+  { _rOMHeaderStartLoc :: ByteString -- 0x00: B <game_code_start>
+  , _rOMHeaderNintendoLogo :: ByteString  -- 0x04: nintendo logo
+  , _rOMHeaderGameTitle :: ByteString  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
+  , _rOMHeaderGameCode :: ByteString  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
+  , _rOMHeaderMakerCode :: ByteString  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
+  , _rOMHeaderMagicByte :: Byte }    -- 0xB2: Must be 0x96
   deriving (Show, Eq)
 
 makeFields ''ROMHeader
