5b757e3e79706657988da6c134d89f0f9698ce03
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-24 02:38:12 +0000

    formatting and lens generation

--- src/Emulator/ROM.hs
+++ src/Emulator/ROM.hs
@@ -2,14 +2,10 @@ module Emulator.ROM where
 
 import Emulator.Types
 
+import Control.Lens
 import Data.ByteString (ByteString)
 
-data ROMHeader = ROMHeader {
-  -- Record                  -- Offset
-  rh_startLoc        :: ByteString, -- 0x00: B <game_code_start>
-  rh_nintendoLogo    :: ByteString,  -- 0x04: nintendo logo
-  rh_gameTitle       :: ByteString,  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
-  rh_gameCode        :: ByteString,  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
-  rh_makerCode       :: ByteString,  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
-  rh_magic           :: Byte    -- 0xB2: Must be 0x96
-} deriving Show
+data ROMHeader = ROMHeader
+  deriving (Show, Eq)
+
+makeFields ''ROMHeader
