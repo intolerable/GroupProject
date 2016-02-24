a8804e586bd2351609327b5223aa4628cc764455
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-24 02:45:05 +0000

    rename magic to magicByte

--- src/Emulator/ROM.hs
+++ src/Emulator/ROM.hs
@@ -11,7 +11,7 @@ data ROMHeader = ROMHeader
   , _romHeaderGameTitle :: ByteString  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
   , _romHeaderGameCode :: ByteString  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
   , _romHeaderMakerCode :: ByteString  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
-  , _romHeaderMagic :: Byte }    -- 0xB2: Must be 0x96
+  , _romHeaderMagicByte :: Byte }    -- 0xB2: Must be 0x96
   deriving (Show, Eq)
 
 makeFields ''ROMHeader
