2deac0d07297d584d8f31b29552e3409db1bf8f4
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-24 02:48:59 +0000

    try to parse a rom, and check for magic byte

---
+++ test/Emulator/ROM/ParserSpec.hs
@@ -0,0 +1,20 @@
+module Emulator.ROM.ParserSpec where
+
+import Emulator.ROM
+import Emulator.ROM.Parser
+
+import Control.Lens
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+  describe "readROM" $ do
+    it "should be able to read a ROM from a file" $ do
+      readROM "./res/suite.gba" >>= \case
+        Left err -> fail err
+        Right (rh, _) -> do
+          rh ^. magicByte `shouldBe` 0x96
