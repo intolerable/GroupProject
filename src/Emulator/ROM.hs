f951e5828909be965fe39b51b37f286ffd0fe77a
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-24 13:21:05 +0000

    unmangle the ROMHeader field names

--- src/Emulator/ROM.hs
+++ src/Emulator/ROM.hs
@@ -6,12 +6,12 @@ import Control.Lens
 import Data.ByteString (ByteString)
 
 data ROMHeader = ROMHeader
-  { _rOMHeaderStartLoc :: ByteString -- 0x00: B <game_code_start>
-  , _rOMHeaderNintendoLogo :: ByteString  -- 0x04: nintendo logo
-  , _rOMHeaderGameTitle :: ByteString  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
-  , _rOMHeaderGameCode :: ByteString  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
-  , _rOMHeaderMakerCode :: ByteString  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
-  , _rOMHeaderMagicByte :: Byte }    -- 0xB2: Must be 0x96
+  { _rhStartLoc :: ByteString -- 0x00: B <game_code_start>
+  , _rhNintendoLogo :: ByteString  -- 0x04: nintendo logo
+  , _rhGameTitle :: ByteString  -- 0xA0: Game title (uppercase ascii, max 12 bytes)
+  , _rhGameCode :: ByteString  -- 0xAC: Game code (uppercase ascii, max 4 bytes)
+  , _rhMakerCode :: ByteString  -- 0xB0: Maker code (uppercase ascii, max 2 bytes)
+  , _rhMagicByte :: Byte }    -- 0xB2: Must be 0x96
   deriving (Show, Eq)
 
-makeFields ''ROMHeader
+makeLensesWith abbreviatedFields ''ROMHeader
