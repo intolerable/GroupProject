3246b743ce5d7d5b7455c4207fb0357d27d6fa51
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-25 17:02:49 +0000

    add another case for instructions

--- src/Emulator/CPU/Instructions/Parser.hs
+++ src/Emulator/CPU/Instructions/Parser.hs
@@ -1,5 +1,8 @@
 module Emulator.CPU.Instructions.Parser where
 
+import Emulator.CPU
+import Emulator.Types
+
 data CPUMode = ARM | THUMB
   deriving (Show, Read, Eq)
 
@@ -11,9 +14,11 @@ type Link = ()
 type Offset = ()
 type RegisterName = ()
 type SetCondition = ()
+type Rotated a = a
 
 data Instruction a where
   Multiply :: Accumulate -> SetCondition -> RegisterName -> RegisterName -> RegisterName -> RegisterName -> Instruction ARM
     -- MUL A S Rd Rn Rs Rm, Rd <- Rm * Rs (+ Rn if A)
   Branch :: Link -> Offset -> Instruction ARM
   BranchExchange :: RegisterName -> Instruction ARM
+  DataProcessing :: Opcode -> SetCondition -> RegisterName -> RegisterName -> Either (Shifted RegisterName) (Rotated Byte) -> Instruction ARM
