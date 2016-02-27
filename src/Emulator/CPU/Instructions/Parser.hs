9119a07bb98214f1b56172d9a07b2496e169e7bb
Fraser Murray <fraser.m.murray@gmail.com>
2016-02-27 16:47:16 +0000

    add constructors for more instructions

--- src/Emulator/CPU/Instructions/Parser.hs
+++ src/Emulator/CPU/Instructions/Parser.hs
@@ -15,10 +15,15 @@ type Offset = ()
 type RegisterName = ()
 type SetCondition = ()
 type Rotated a = a
+type Signed = ()
+type Granularity = ()
 
 data Instruction a where
+  DataProcessing :: Opcode -> SetCondition -> RegisterName -> RegisterName -> Either (Shifted RegisterName) (Rotated Byte) -> Instruction ARM
   Multiply :: Accumulate -> SetCondition -> RegisterName -> RegisterName -> RegisterName -> RegisterName -> Instruction ARM
     -- MUL A S Rd Rn Rs Rm, Rd <- Rm * Rs (+ Rn if A)
-  Branch :: Link -> Offset -> Instruction ARM
+  MultiplyLong :: Signed -> Accumulate -> SetCondition -> RegisterName -> RegisterName -> RegisterName -> RegisterName -> Instruction ARM
+  SingleDataSwap :: Granularity -> RegisterName -> RegisterName -> RegisterName -> Instruction ARM
   BranchExchange :: RegisterName -> Instruction ARM
-  DataProcessing :: Opcode -> SetCondition -> RegisterName -> RegisterName -> Either (Shifted RegisterName) (Rotated Byte) -> Instruction ARM
+  Branch :: Link -> Offset -> Instruction ARM
+  Undefined :: Instruction ARM
