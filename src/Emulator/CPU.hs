a8b614002658f37a634e9aadc5ba2ed9cbabd1dc
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-07 00:47:43 +0000

    remove RegisterX

--- src/Emulator/CPU.hs
+++ src/Emulator/CPU.hs
@@ -15,34 +15,6 @@ data CPUMode = User
              | System
   deriving (Show, Read, Eq)
 
-data RegisterX =
-  -- Registers R0 - R12 are all general purpose registers
-  -- In non-thumb mode all registers are available for any purpose
-  -- In thumb mode only registers R0 - R7 are available, whereas
-  --  R8 - R12 are only accessible from certain instructions
-  R0 RegisterVal
-  | R1 RegisterVal
-  | R2 RegisterVal
-  | R3 RegisterVal
-  | R4 RegisterVal
-  | R5 RegisterVal
-  | R6 RegisterVal
-  | R7 RegisterVal
-  | R8 RegisterVal
-  | R9 RegisterVal
-  | R10 RegisterVal
-  | R11 RegisterVal
-  | R12 RegisterVal
-  | R13 RegisterVal  -- SP Register
-                     -- Stack pointer in thumb mode, SP or GP in ARM mode
-  | R14 RegisterVal  -- LR Register
-                     -- Return address after branch with
-                     -- link (LB instruction), or GP in ARM
-  | R15 RegisterVal  -- PC Register
-                     -- Program counter, contains address to currently executing
-                     -- statement.
-  | CPSR RegisterVal -- Flags and CPU state register
-
 data Flags = Flags -- Status Register
   { _flagsSign :: Bool
   , _flagsZero :: Bool
