7509f034be6c4a493970f1530533e9b2ded161cf
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-11 00:42:40 +0000

    tests for bitmask macro

---
+++ test/Utilities/Parser/TemplateHaskellSpec.hs
@@ -0,0 +1,17 @@
+module Utilities.Parser.TemplateHaskellSpec where
+
+import Test.Hspec
+
+import Utilities.Parser.TemplateHaskell
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+  describe "bitmask" $ do
+    it "should be able to generate a basic bitmask" $ do
+      $(bitmask 1 0) (0b1001 :: Word) `shouldBe` (0b01 :: Word)
+      $(bitmask 3 2) (0b1001 :: Word) `shouldBe` (0b10 :: Word)
+      $(bitmask 3 0) (0b01011010 :: Word) `shouldBe` (0b1010 :: Word)
