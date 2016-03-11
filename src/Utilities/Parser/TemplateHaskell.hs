7bb652de000fd02c814f0ea7c89e4ad3824153cd
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-11 00:41:45 +0000

    add a TH macro for generating a bitmask

---
+++ src/Utilities/Parser/TemplateHaskell.hs
@@ -0,0 +1,19 @@
+module Utilities.Parser.TemplateHaskell where
+
+import Data.Bits
+import Language.Haskell.TH
+
+-- $(bitmask x y) :: (Integral a, Bits a, Num b) => a -> b
+bitmask :: Word -> Word -> Q Exp
+bitmask x y = do
+  varName <- newName "w"
+  -- \w -> fromIntegral ((w .&. mask x y) `shiftR` y)
+  return $ LamE [VarP varName] $
+    VarE 'fromIntegral $$ InfixE (Just $ InfixE (Just $ VarE varName) (VarE '(.&.)) (Just $ LitE $ IntegerL $ fromIntegral mask)) (VarE 'shiftR) (Just $ LitE $ IntegerL $ fromIntegral y)
+  where
+    ($$) = AppE
+    a, b :: Word
+    a = 1 `shiftL` fromIntegral x
+    b = 1 `shiftL` fromIntegral y
+    mask :: Word
+    mask = ((a - 1) .&. complement (b - 1)) .|. a
