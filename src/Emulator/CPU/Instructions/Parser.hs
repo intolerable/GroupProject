108f443c503f024dbf2cc0a5887d5b163886e7e0
Cameron Diver <cameron.diver94@gmail.com>
2016-03-12 14:53:59 +0000

    Fixed immediate rotation value code
    It required the shift to be 2 times that of the one in the instruction

--- src/Emulator/CPU/Instructions/Parser.hs
+++ src/Emulator/CPU/Instructions/Parser.hs
@@ -228,7 +228,8 @@ parseOperand2 :: Immediate -> MWord -> Either (Shifted RegisterName) (Rotated By
 parseOperand2 (Immediate False) w =
   Left $ parseShiftedRegister w
 parseOperand2 (Immediate True) w =
-  Right $ Rotated (fromIntegral $ w .&. 0xF00 `shiftR` 8) (fromIntegral (w .&. 0xFF))
+  -- The value must be rotated left 2*the shift value in instruction
+  Right $ Rotated (fromIntegral $ 2*((w .&. 0xF00) `shiftR` 8)) (fromIntegral (w .&. 0xFF))
 
 parseShiftedRegister :: MWord -> Shifted RegisterName
 parseShiftedRegister w =
