e031eb21eb68e03e97db958800d89dbc04652313
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-13 00:14:39 +0000

    drop fromIntegral on bitmask splice, keep the same type

--- src/Utilities/Parser/TemplateHaskell.hs
+++ src/Utilities/Parser/TemplateHaskell.hs
@@ -4,16 +4,15 @@ import Control.Monad
 import Data.Bits
 import Language.Haskell.TH
 
--- $(bitmask x y) :: (Integral a, Bits a, Num b) => a -> b
+-- $(bitmask x y) :: (Integral a, Bits a, Num a) => a -> a
 bitmask :: Word -> Word -> Q Exp
 bitmask x y = do
   when (y > x) $ fail "Invalid bitmask: the lower bound for a bitmask must be less than its upper bound."
   varName <- newName "w"
   -- \w -> fromIntegral ((w .&. mask x y) `shiftR` y)
   return $ LamE [VarP varName] $
-    VarE 'fromIntegral $$ InfixE (Just $ InfixE (Just $ VarE varName) (VarE '(.&.)) (Just $ LitE $ IntegerL $ fromIntegral mask)) (VarE 'shiftR) (Just $ LitE $ IntegerL $ fromIntegral y)
+    InfixE (Just $ InfixE (Just $ VarE varName) (VarE '(.&.)) (Just $ LitE $ IntegerL $ fromIntegral mask)) (VarE 'shiftR) (Just $ LitE $ IntegerL $ fromIntegral y)
   where
-    ($$) = AppE
     a, b :: Word
     a = 1 `shiftL` fromIntegral x
     b = 1 `shiftL` fromIntegral y
