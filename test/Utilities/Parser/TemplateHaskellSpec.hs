d88062e236a8ec8aab724319152d0c6d704b14d3
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-13 00:15:06 +0000

    update tests for unnecessary type annotations

--- test/Utilities/Parser/TemplateHaskellSpec.hs
+++ test/Utilities/Parser/TemplateHaskellSpec.hs
@@ -12,6 +12,6 @@ spec = do
 
   describe "bitmask" $ do
     it "should be able to generate a basic bitmask" $ do
-      $(bitmask 1 0) (0b1001 :: Word) `shouldBe` (0b01 :: Word)
-      $(bitmask 3 2) (0b1001 :: Word) `shouldBe` (0b10 :: Word)
-      $(bitmask 3 0) (0b01011010 :: Word) `shouldBe` (0b1010 :: Word)
+      $(bitmask 1 0) (0b1001 :: Word) `shouldBe` 0b01
+      $(bitmask 3 2) (0b1001 :: Word) `shouldBe` 0b10
+      $(bitmask 3 0) (0b01011010 :: Word) `shouldBe` 0b1010
