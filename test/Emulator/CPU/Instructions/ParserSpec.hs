46b3b260855e576076d66aaec86dd814e309d41b
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-19 15:45:58 +0000

    remove whitespace

--- test/Emulator/CPU/Instructions/ParserSpec.hs
+++ test/Emulator/CPU/Instructions/ParserSpec.hs
@@ -18,7 +18,7 @@ spec = do
       --parseARM 0xE28BA028 `shouldBe` Right (AL, DataProcessing TST (SetCondition False) (RegisterName 22) (RegisterName 20) (Right (Rotated 8 40)))
       parseARM 0xEA00007F `shouldBe` Right (AL, Branch False 508)
       parseARM 0xE59FD028 `shouldBe` Right (AL, SingleDataTransfer Pre Up Word False Load (RegisterName 15) (RegisterName 13) (Right 40))
-      parseARM 0xE5810000 `shouldBe` Right (AL, SingleDataTransfer Pre Up Word False Store (RegisterName 1) (RegisterName 0) (Right   0))
+      parseARM 0xE5810000 `shouldBe` Right (AL, SingleDataTransfer Pre Up Word False Store (RegisterName 1) (RegisterName 0) (Right 0))
       parseARM 0xE12FFF11 `shouldBe` Right (AL, BranchExchange (RegisterName 1))
       parseARM 0xE1D310B8 `shouldBe` Right (AL, HalfwordDataTransferImmediate Pre Up False Load False HalfWord (RegisterName 3) (RegisterName 1) 8)
       parseARM 0xE2833C02 `shouldBe` Right (AL, DataProcessing ADD (SetCondition False) (RegisterName 3) (RegisterName 3) $ Right (Rotated 24 2))
