87a4168ae2c21d1b830bfa2c3d49e434aeb57a4a
Harry Duce <harry.duce@gmail.com>
2016-03-19 15:30:59 +0000

    Add DisplayControl record

--- src/Emulator/Video/VideoController.hs
+++ src/Emulator/Video/VideoController.hs
@@ -1,5 +1,25 @@
 module Emulator.Video.VideoController where
 
--- import Emulator.Memory.Video
--- import Emulator.Types
--- import Utilities.Parser.TemplateHaskell
+import Emulator.Memory.Video
+import Emulator.Types
+import Utilities.Parser.TemplateHaskell
+
+data DisplayControl =
+  DisplayControl { bgMode :: Byte
+                 , reservedMode :: Bool
+                 , displayFrameSelect :: Bool
+                 , hblankInterval :: Bool
+                 , objCharacterVRAMMapping :: Bool
+                 , forcedBlank :: Bool
+                 , screenDispBG0 :: Bool
+                 , screenDispBG1 :: Bool
+                 , screenDispBG2 :: Bool
+                 , screenDispBG3 :: Bool
+                 , screenDispBGOBJ :: Bool
+                 , window0DispFlag :: Bool
+                 , window1DispFlag :: Bool
+                 , windowOBJDispFlag :: Bool }
+  deriving (Show, Read, Eq)
+
+readDisplayControl :: HalfWord -> DisplayControl
+readDisplayControl _ = undefined
