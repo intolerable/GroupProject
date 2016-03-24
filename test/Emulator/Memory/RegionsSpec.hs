a9ca6c524ea35101a58724fbdea579a522a7f048
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-24 16:47:22 +0000

    fix tests

--- test/Emulator/Memory/RegionsSpec.hs
+++ test/Emulator/Memory/RegionsSpec.hs
@@ -11,8 +11,8 @@ spec :: Spec
 spec = do
   describe "Region from address" $ do
     it "should correctly discretise memory addresses" $ do
-      addressToRegionType 0x00000100 `shouldBe` BIOS
-      addressToRegionType 0x00004001 `shouldBe` Unused
-      addressToRegionType 0x06000000 `shouldBe` VRAM
-      addressToRegionType 0x0E00FFFF `shouldBe` GamePakSRAM
-      addressToRegionType 0xFFFFFFFF `shouldBe` Unused
+      addressToRegionType 0x00000100 `shouldBe` (0x00000000, BIOS)
+      addressToRegionType 0x00004001 `shouldBe` (0x00004000, Unused)
+      addressToRegionType 0x06000000 `shouldBe` (0x06000000, VRAM)
+      addressToRegionType 0x0E00FFFF `shouldBe` (0x0E000000, GamePakSRAM)
+      addressToRegionType 0xFFFFFFFF `shouldBe` (0x0E010000, Unused)
