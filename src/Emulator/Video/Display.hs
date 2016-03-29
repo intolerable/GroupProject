3ae7bde08e0b6ddc535783311d6e3b90fee0b0fa
Harry Duce <harry.duce@gmail.com>
2016-03-29 18:28:21 +0100

    Change gl coords to match size in pixel

--- src/Emulator/Video/Display.hs
+++ src/Emulator/Video/Display.hs
@@ -1,13 +1,9 @@
 module Emulator.Video.Display where
 
 import Codec.Picture
-import Control.Monad.IO.Class
-import Data.Array.Storable
-import Data.ByteString (ByteString)
 import Data.Vector.Storable
 import Data.Word
 import Graphics.Rendering.OpenGL
---import qualified Data.StateVar as StateVar
 import qualified Graphics.UI.GLUT as GLUT
 
 type FileName = String
@@ -24,16 +20,17 @@ animate = do
   textureBinding Texture2D $= Just name
   textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
   renderPrimitive Quads $ do
+    texCoord $ TexCoord2 0 (1 :: GLdouble)
+    vertex $ Vertex2 0 (127 :: GLdouble)
     texCoord $ TexCoord2 0 (0 :: GLdouble)
     vertex $ Vertex2 0 (0 :: GLdouble)
     texCoord $ TexCoord2 1 (0 :: GLdouble)
-    vertex $ Vertex2 1 (0 :: GLdouble)
+    vertex $ Vertex2 127 (0 :: GLdouble)
     texCoord $ TexCoord2 1 (1 :: GLdouble)
-    vertex $ Vertex2 1 (1 :: GLdouble)
-    texCoord $ TexCoord2 0 (1 :: GLdouble)
-    vertex $ Vertex2 0 (1 :: GLdouble)
+    vertex $ Vertex2 127 (127 :: GLdouble)
   GLUT.swapBuffers
 
+-- Temp. function for reading in BMP files
 loadTestTexture :: FileName -> IO TextureObject
 loadTestTexture fname = do
   bmp <- readBitmap fname
@@ -41,11 +38,14 @@ loadTestTexture fname = do
     Right bmp' -> do
       let (w, h, pixVec) = parseImage bmp'
       loadTexture pixVec w h
+    Left _ -> undefined lines
   where
     parseImage :: DynamicImage -> (Int, Int, Vector Word8)
     parseImage (ImageRGB8 (Image w h pixData)) = do
       (w, h, pixData)
+    parseImage _ = undefined lines
 
+-- Takes Storable Vector8 and loads it to a 2D Texture
 loadTexture :: Vector Word8 -> Int -> Int -> IO TextureObject
 loadTexture vec w h = unsafeWith vec $ \ptr -> do
     name <- genObjectName
