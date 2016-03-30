8ae7d3fdb1669fdf4f7a6779747d49e20094e738
Harry Duce <harry.duce@gmail.com>
2016-03-30 12:52:43 +0100

    Add drawBackground function

--- src/Emulator/Video/Display.hs
+++ src/Emulator/Video/Display.hs
@@ -16,19 +16,27 @@ display = do
 animate :: GLUT.IdleCallback
 animate = do
   clear [GLUT.ColorBuffer]
-  name <- loadTestTexture "/Users/harryduce/Downloads/star.bmp"
-  textureBinding Texture2D $= Just name
+  bg0 <- drawBackground "/Users/harryduce/4thYrProj/bmp/BLU.bmp" (0, 240) (0, 160)
+  bg1 <- drawBackground "/Users/harryduce/4thYrProj/bmp/RED.bmp" (0, 240) (30, 160)
+  bg2 <- drawBackground "/Users/harryduce/4thYrProj/bmp/GRN.bmp" (0, 240) (60, 160)
+  obj <- drawBackground "/Users/harryduce/4thYrProj/bmp/paper_mario.bmp" (50, 80) (120, 160)
+  obj2 <- drawBackground "/Users/harryduce/4thYrProj/bmp/toad.bmp" (150, 230) (100, 160)
+  GLUT.swapBuffers
+
+drawBackground :: FileName -> (GLdouble, GLdouble) -> (GLdouble, GLdouble) -> IO TextureObject
+drawBackground fname (x1, x2) (y1, y2) = do
+  bg <- loadTestTexture fname
   textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
   renderPrimitive Quads $ do
-    texCoord $ TexCoord2 0 (1 :: GLdouble)
-    vertex $ Vertex2 0 (127 :: GLdouble)
     texCoord $ TexCoord2 0 (0 :: GLdouble)
-    vertex $ Vertex2 0 (0 :: GLdouble)
+    vertex $ Vertex2 x1 (y1 :: GLdouble)
     texCoord $ TexCoord2 1 (0 :: GLdouble)
-    vertex $ Vertex2 127 (0 :: GLdouble)
+    vertex $ Vertex2 x2 (y1 :: GLdouble)
     texCoord $ TexCoord2 1 (1 :: GLdouble)
-    vertex $ Vertex2 127 (127 :: GLdouble)
-  GLUT.swapBuffers
+    vertex $ Vertex2 x2 (y2 :: GLdouble)
+    texCoord $ TexCoord2 0 (1 :: GLdouble)
+    vertex $ Vertex2 x1 (y2 :: GLdouble)
+  return bg
 
 -- Temp. function for reading in BMP files
 loadTestTexture :: FileName -> IO TextureObject
@@ -46,6 +54,7 @@ loadTestTexture fname = do
     parseImage _ = undefined lines
 
 -- Takes Storable Vector8 and loads it to a 2D Texture
+-- Assigns textureBinding to texture object
 loadTexture :: Vector Word8 -> Int -> Int -> IO TextureObject
 loadTexture vec w h = unsafeWith vec $ \ptr -> do
     name <- genObjectName
