e46deb6c6eaab3bea9a10dba34d27073434bb094
Fraser Murray <fraser.m.murray@gmail.com>
2016-03-31 22:58:56 +0100

    the beginnings of documentation

--- src/Emulator.hs
+++ src/Emulator.hs
@@ -1,4 +1,6 @@
-module Emulator where
+module Emulator
+  ( main
+  , loadROM ) where
 
 import Emulator.CPU
 import Emulator.CPU.Instructions.Parser
@@ -16,11 +18,18 @@ import Control.Monad.Trans.State
 import qualified Data.ByteString as BS
 import qualified Graphics.UI.GLUT as GLUT
 
+-- | Launches the emulator. Currently only initializes GL and fires off the main loop.
+--     In the future, this will parse command line arguments so that the emulator
+--     can be run in headless mode \/ debug mode \/ with various other options.
 main :: IO ()
 main = do
   initGL
   GLUT.mainLoop
 
+-- | Initialize OpenGL and set up the window ready to render sprite- and tile-based 2D
+--     graphics. We enable double-buffering and alpha modes, create a window with a
+--     modified ortho (in order to match GBA device pixels to GL units), and then
+--     link the functions for the display and idle loops.
 initGL :: IO ()
 initGL = do
   _ <- GLUT.getArgsAndInitialize
@@ -36,12 +45,15 @@ initGL = do
   GLUT.displayCallback $= display
   GLUT.idleCallback $= Just animate
 
+-- | Callback that should be executed whenever the window is resized. We just fix the
+--     ortho to 240x160 and set the correct window size.
 reshape :: GLUT.ReshapeCallback
 reshape (Size x y) = do
   loadIdentity
   ortho 0 240 160 0 0 1
   viewport $= (GLUT.Position 0 0, Size x y)
 
+-- | Load a ROM from a given file path, and then start executing the ROM.
 loadROM :: FilePath -> IO ()
 loadROM fp =
   readROM fp >>= \case
