b0f2bcf506c7b43ecac334bf9840bf5292417816
Harry Duce <harry.duce@gmail.com>
2016-04-07 11:43:58 +0100

    Add drawHline and drawVLines functions for drawing tiles

--- src/Emulator/Video/TileModes.hs
+++ src/Emulator/Video/TileModes.hs
@@ -17,17 +17,46 @@ mode0 :: AddressSpace m => LCDControl -> m ()
 mode0 = do
   return bg0
 
+-- Text Mode
 bg0 :: AddressSpace m => m ()
 bg0 = do
   bg <- recordBGControl 0x04000008
-  let tileBaseBlock = characterBaseBlock bg
-  let tileData = tileBaseBlock + 0x06000000
-  -- tileMemOffset <- characterBaseBlock bg
+  bgOffset <- recordBGOffset 0x04000010 0x04000020
+  let tileBase = characterBaseBlock bg
+  let tileData = getTileBlock tileBase
   return ()
 
-drawRow :: Int -> (GLdouble, GLdouble) -> Address -> IO ()
-drawRow (-1) _ _ = return ()
-drawRow n (x, y) addr = do
-  drawTile' addr (x, x+8) (y, y+8)
-  drawRow (n-1) (x,y) (addr + 0x00000020)
+drawVLines :: String -> Int -> Int -> (GLdouble, GLdouble) -> Address -> IO ()
+drawVLines _ 0 _ _ _ = return ()
+drawVLines fname n columns (x, y) addr = do
+  drawHLine fname columns (x, y) addr
+  drawVLines fname (n-1) columns (x, y+8) addr
 
+drawHLine :: String -> Int -> (GLdouble, GLdouble) -> Address -> IO ()
+drawHLine _ (0) _ _ = return ()
+drawHLine fname n (x, y) addr = do
+  drawTile fname (x, x+8) (y, y+8)
+  drawHLine fname (n-1) (x+8,y) (addr + 0x00000020)
+
+getTileBlock :: Byte -> Address
+getTileBlock tileBase
+  | tileBase == 0 = 0x06000000
+  | tileBase == 1 = 0x06004000
+  | tileBase == 2 = 0x06008000
+  | otherwise = 0x0600C000
+
+-- Returns number of tiles to be drawn
+textBGSize :: Byte -> (Int, Int)
+textBGSize byt
+  | byt == 0 = (32, 32)
+  | byt == 1 = (64, 32)
+  | byt == 2 = (32, 64)
+  | otherwise = (64, 32)
+
+-- Returns number of tiles to be drawn
+affineBGSize :: Byte -> (Int, Int)
+affineBGSize byt
+  | byt == 0 = (16, 16)
+  | byt == 1 = (32, 32)
+  | byt == 2 = (64, 64)
+  | otherwise = (128, 128)
