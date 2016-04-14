db66103f7ee443abe82589f0c7e3b817fd223245
Harry Duce <harry.duce@gmail.com>
2016-04-14 13:55:33 +0100

    Return a StorableArray of pixelData for a 4bpp tile

--- src/Emulator/Video/TileModes.hs
+++ src/Emulator/Video/TileModes.hs
@@ -6,6 +6,7 @@ import Emulator.Video.VideoController
 
 import Control.Monad.IO.Class
 import Data.Array.IArray
+import Data.Array.MArray
 import Data.Array.Storable
 import Data.Bits
 import Graphics.Rendering.OpenGL
@@ -133,9 +134,28 @@ pixelData :: AddressIO m => PixFormat -> Palette -> Tile -> Address -> m (Storab
 -- 256/1 palette format
 pixelData True _palette _tile _ = undefined
 -- 16/16 palette format
-pixelData _ palette _tile palBank = do
-  let _bank = ixmap (0x05000000 + palBank, (0x05000000 + palBank + 0x0000001F)) (id) palette :: Palette
-  undefined
+pixelData _ palette tile palBank = do
+  let bank = ixmap (palBankAddr, (palBankAddr + 0x0000001F)) (id) palette :: Palette
+  let tilePixelDataList = palette16 bank tile palBankAddr (fst tileBounds) 0
+  tilePixelData <- liftIO $ newListArray tileBounds tilePixelDataList
+  return tilePixelData
+  where
+    tileBounds = bounds tile
+    palBankAddr = 0x05000000 + palBank
+
+palette16 :: Palette -> Tile -> Address -> Address -> Int -> [HalfWord]
+palette16 _ _ _ _ 32 = []
+palette16 bank tile palBankBaseAddr tileAddr n = col1:col2:palette16 bank tile palBankBaseAddr tileAddr (n+1)
+  where
+    byt = tile!(tileAddr + (0x00000001 * fromIntegral n))
+    nib1 = fromIntegral $ $(bitmask 3 0) byt :: Address
+    nib2 = fromIntegral $ $(bitmask 7 4) byt :: Address
+    col1Byt1 = bank!(nib1 + palBankBaseAddr)
+    col1Byt2 = bank!(nib1 + palBankBaseAddr + 0x00000001)
+    col1 = ((fromIntegral col1Byt2 :: HalfWord) `shiftL` 8) .|. ((fromIntegral col1Byt1 :: HalfWord) .&. 0xFF) :: HalfWord
+    col2Byt1 = bank!(nib2 + palBankBaseAddr)
+    col2Byt2 = bank!(nib2 + palBankBaseAddr + 0x00000001)
+    col2 = ((fromIntegral col2Byt2 :: HalfWord) `shiftL` 8) .|. ((fromIntegral col2Byt1 :: HalfWord) .&. 0xFF) :: HalfWord
 
 -- a is the upper byte, b is the lower
 parseScreenEntry :: Byte -> Byte -> PixFormat -> TileSetBaseAddress -> ScreenEntry
