b89fd1272f6a6d25a95f540eab7ed1eab4258ba6
Harry Duce <harry.duce@gmail.com>
2016-04-15 12:28:16 +0100

    Recurse through all 128 objects inside OAM and pass to parseObject function

--- src/Emulator/Video/Sprite.hs
+++ src/Emulator/Video/Sprite.hs
@@ -7,19 +7,27 @@ import Emulator.Video.Util
 import Data.Array.IArray
 
 type MappingMode = Bool
+type OAM = Array Address Byte
 
 readOAM :: AddressIO m => MappingMode -> m ()
-readOAM _mapMode = do
-  _oam <- readRange (0x07000000, 0x070003FF)
+readOAM mapMode = do
+  oam <- readRange (0x07000000, 0x070003FF)
+  recurseOAM oam mapMode 0
   return ()
 
 -- Access each object
-recurseOAM :: AddressIO m => Array Address Byte -> MappingMode -> m ()
-recurseOAM _oam _mapMode = undefined
+recurseOAM :: AddressIO m => OAM -> MappingMode -> Int -> m ()
+recurseOAM _ _ 128 = return ()
+recurseOAM oam mapMode n = do
+  let obj = ixmap (objAddr, objAddr + 0x00000005) (id) oam
+  parseObjectAttr obj mapMode
+  recurseOAM oam mapMode (n+1)
+  where
+    objAddr = 0x07000000 + 0x00000008 * (fromIntegral n)
 
 -- Access attributes of object
-parseObject :: AddressIO m => Array Address Byte -> m ()
-parseObject _obj = undefined
+parseObjectAttr :: AddressIO m => Array Address Byte -> Bool -> m ()
+parseObjectAttr _obj _mapMode = undefined
 
 attribute0 :: Byte -> Byte -> HalfWord
 attribute0 _ _ = undefined
