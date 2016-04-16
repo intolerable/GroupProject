ae2a1b58777dc13acc5ad82e15f8b6a55c724cc4
Fraser Murray <fraser.m.murray@gmail.com>
2016-04-16 22:16:27 +0100

    add a padded show hex function

--- src/Utilities/Show.hs
+++ src/Utilities/Show.hs
@@ -1,6 +1,7 @@
 module Utilities.Show
   ( showBinary
-  , showHex ) where
+  , showHex
+  , showHexPadded ) where
 
 import Data.Char
 import qualified Numeric
@@ -10,3 +11,8 @@ showBinary x = "0b" ++ Numeric.showIntAtBase 2 (toUpper . intToDigit) x ""
 
 showHex :: (Integral a, Show a) => a -> String
 showHex x = "0x" ++ Numeric.showIntAtBase 16 (toUpper . intToDigit) x ""
+
+showHexPadded :: (Integral a, Show a) => Int -> a -> String
+showHexPadded n x = "0x" ++ replicate (n - length numStr) '0' ++ numStr
+  where
+    numStr = Numeric.showIntAtBase 16 (toUpper . intToDigit) x ""
