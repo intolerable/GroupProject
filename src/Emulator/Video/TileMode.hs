0eb5dbe21dddd9dbd3b628bb7ecfccbca72682c0
Harry Duce <harry.duce@gmail.com>
2016-05-04 15:46:18 +0100

    Edit drawTextBG function to take correct parameters

--- src/Emulator/Video/TileMode.hs
+++ src/Emulator/Video/TileMode.hs
@@ -65,33 +65,33 @@ baseTileMapAddr mapBase = 0x06000000 + (0x00000800 * (fromIntegral mapBase))
 drawTextBG :: AddressIO m => Int -> PixFormat -> TileMapBaseAddress -> TileSetBaseAddress -> TileOffset -> Palette -> m ()
 drawTextBG 0 pixFormat tileMapAddr tileSetAddr offSet palette = do
   tileMap0 <- readTileMap tileMapAddr
-  tileSet <- readCharBlocks tileSetAddr False
-  drawTileMap 32 pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
+  tileSet <- readCharBlocks tileSetAddr pixFormat
+  drawTileMap (32, 32) pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
   return ()
 drawTextBG 1 pixFormat tileMapAddr tileSetAddr offSet@(xOff, yOff) palette = do
   tileMap0 <- readTileMap tileMapAddr
   tileMap1 <- readTileMap (tileMapAddr + 0x00000800)
-  tileSet <- readCharBlocks tileSetAddr False
-  drawTileMap 32 pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
-  drawTileMap 32 pixFormat tileMap1 tileSet (xOff + 32, yOff) palette tileMapAddr tileSetAddr
+  tileSet <- readCharBlocks tileSetAddr pixFormat
+  drawTileMap (32, 32) pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
+  drawTileMap (32, 32) pixFormat tileMap1 tileSet (xOff + 32, yOff) palette tileMapAddr tileSetAddr
   return ()
 drawTextBG 2 pixFormat tileMapAddr tileSetAddr offSet@(xOff, yOff) palette = do
   tileMap0 <- readTileMap tileMapAddr
   tileMap1 <- readTileMap (tileMapAddr + 0x00000800)
-  tileSet <- readCharBlocks tileSetAddr False
-  drawTileMap 32 pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
-  drawTileMap 32 pixFormat tileMap1 tileSet (xOff, yOff + 32) palette tileMapAddr tileSetAddr
+  tileSet <- readCharBlocks tileSetAddr pixFormat
+  drawTileMap (32, 32) pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
+  drawTileMap (32, 32) pixFormat tileMap1 tileSet (xOff, yOff + 32) palette tileMapAddr tileSetAddr
   return ()
 drawTextBG _ pixFormat tileMapAddr tileSetAddr offSet@(xOff, yOff) palette = do
   tileMap0 <- readTileMap tileMapAddr
   tileMap1 <- readTileMap (tileMapAddr + 0x00000800)
   tileMap2 <- readTileMap (tileMapAddr + 0x00001000)
   tileMap3 <- readTileMap (tileMapAddr + 0x00001800)
-  tileSet <- readCharBlocks tileSetAddr False
-  drawTileMap 32 pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
-  drawTileMap 32 pixFormat tileMap1 tileSet (xOff + 32, yOff) palette tileMapAddr tileSetAddr
-  drawTileMap 32 pixFormat tileMap2 tileSet (xOff, yOff + 32) palette tileMapAddr tileSetAddr
-  drawTileMap 32 pixFormat tileMap3 tileSet (xOff + 32, yOff + 32) palette tileMapAddr tileSetAddr
+  tileSet <- readCharBlocks tileSetAddr pixFormat
+  drawTileMap (32, 32) pixFormat tileMap0 tileSet offSet palette tileMapAddr tileSetAddr
+  drawTileMap (32, 32) pixFormat tileMap1 tileSet (xOff + 32, yOff) palette tileMapAddr tileSetAddr
+  drawTileMap (32, 32) pixFormat tileMap2 tileSet (xOff, yOff + 32) palette tileMapAddr tileSetAddr
+  drawTileMap (32, 32) pixFormat tileMap3 tileSet (xOff + 32, yOff + 32) palette tileMapAddr tileSetAddr
   return ()
 
 readTileMap :: AddressSpace m => Address -> m (TileMap)
@@ -102,11 +102,11 @@ readCharBlocks addr False = readRange (addr, addr + 0x00007FFF)
 readCharBlocks addr True = readRange (addr, addr + 0x0000FFFF)
 
 -- Draw 32x32 tiles at a time
-drawTileMap :: AddressIO m => Int -> PixFormat -> TileMap -> TileSet -> TileOffset -> Palette -> TileMapBaseAddress -> TileSetBaseAddress -> m ()
-drawTileMap 0 _ _ _ _ _ _ _ = return ()
-drawTileMap rows pixFormat tileMap tileSet bgOffset@(xOff, yOff) palette baseAddr setBaseAddr = do
-  drawHLine 32 baseAddr pixFormat tileMapRow tileSet bgOffset palette setBaseAddr
-  drawTileMap (rows-1) pixFormat tileMap tileSet (xOff, yOff + 8) palette (baseAddr + 0x00000040) setBaseAddr
+drawTileMap :: AddressIO m => (Int, Int) -> PixFormat -> TileMap -> TileSet -> TileOffset -> Palette -> TileMapBaseAddress -> TileSetBaseAddress -> m ()
+drawTileMap (0, _) _ _ _ _ _ _ _ = return ()
+drawTileMap (rows, cols) pixFormat tileMap tileSet bgOffset@(xOff, yOff) palette baseAddr setBaseAddr = do
+  drawHLine cols baseAddr pixFormat tileMapRow tileSet bgOffset palette setBaseAddr
+  drawTileMap (rows-1, cols) pixFormat tileMap tileSet (xOff, yOff + 8) palette (baseAddr + 0x00000040) setBaseAddr
   return ()
   where
     tileMapRow = ixmap (baseAddr, baseAddr + 0x0000003F) (id) tileMap :: TileMap
