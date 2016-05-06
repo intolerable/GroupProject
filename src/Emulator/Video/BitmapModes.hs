d315c0d0ea8ee2e7ae0192f12cf92bbc9b9201f0
Harry Duce <harry.duce@gmail.com>
2016-05-06 23:53:07 +0100

    Change bitmapModes function to separate reading from memory and storing ScreenObj

--- src/Emulator/Video/BitmapModes.hs
+++ src/Emulator/Video/BitmapModes.hs
@@ -13,23 +13,27 @@ import Data.Array.Storable
 
 type Bitmap = Array Address Byte
 
-bitmapModes :: AddressIO m => LCDControl -> m ()
+bitmapModes :: AddressIO m => LCDControl -> m (ScreenObj)
 bitmapModes cnt = do
   bgCNT <- recordBGControl 0x0400000C
   xWord <- readAddressWord 0x04000028
   yWord <- readAddressWord 0x0400002C
   paramMem <- readRange (0x04000020, 0x04000027)
+  vram <- readBitmapVram (bgMode cnt) (displayFrameSelect cnt)
+  palette <- readRange (0x05000000, 0x050001FF)
   let refPoint = (referencePoint xWord, referencePoint yWord)
   let params = affineParameters 0x04000020 0x04000022 0x04000024 0x04000026 paramMem
   case bgMode cnt of
-    3 -> mode3n5 bgCNT refPoint params (240, 160) (0x06000000, 0x06012BFF)
-    4 -> if displayFrameSelect cnt
-      then mode4 bgCNT refPoint params (240, 160) (0x0600A000, 0x060135FF)
-      else mode4 bgCNT refPoint params (240, 160) (0x06000000, 0x0060095FF)
-    _ -> if displayFrameSelect cnt
-      then mode3n5 bgCNT refPoint params (160, 128) (0x0600A000, 0x006013FFF)
-      else mode3n5 bgCNT refPoint params (160, 128) (0x06000000, 0x006009FFF)
-  return ()
+    3 -> return (mode3n5 bgCNT refPoint params (240, 160) vram)
+    4 -> return (mode4 bgCNT refPoint params (240, 160) vram palette)
+    _ -> return (mode3n5 bgCNT refPoint params (160, 128) vram)
+
+readBitmapVram :: AddressSpace m => Byte -> Bool -> m (Array Address Byte)
+readBitmapVram 3 _ = readRange (0x06000000, 0x06012BFF)
+readBitmapVram 4 True = readRange (0x0600A000, 0x060135FF)
+readBitmapVram 4 False = readRange (0x06000000, 0x0060095FF)
+readBitmapVram _ True = readRange (0x0600A000, 0x006013FFF)
+readBitmapVram _ False = readRange (0x06000000, 0x006009FFF)
 
 mode3n5 :: AddressIO m => BGControl -> AffineRefPoints -> AffineParameters -> (Int, Int) -> (Address, Address) -> m ()
 mode3n5 _bgCNT ref@(x, y) params (w, h) vramAddr = do
