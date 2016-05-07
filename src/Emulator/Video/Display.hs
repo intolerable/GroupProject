d38642dacbd3e0139e4fad384428b918d21546fb
Harry Duce <harry.duce@gmail.com>
2016-05-07 15:23:22 +0100

    Store, concat and draw ScreenObj data

--- src/Emulator/Video/Display.hs
+++ src/Emulator/Video/Display.hs
@@ -1,8 +1,10 @@
 module Emulator.Video.Display where
 
---import Emulator.Video.BitmapModes
+import Emulator.Video.BitmapModes
+import Emulator.Video.Renderer
 import Emulator.Video.Sprite
 import Emulator.Video.TileMode
+import Emulator.Video.Util
 import Emulator.Video.VideoController
 
 import Emulator.Interpreter.Monad
@@ -11,6 +13,7 @@ import Control.Concurrent.STM
 import Control.Concurrent.STM.TXChan
 import Control.Monad
 import Control.Monad.IO.Class
+import Data.List
 import Graphics.Rendering.OpenGL
 import qualified Graphics.UI.GLUT as GLUT
 
@@ -28,8 +31,10 @@ animate' chan = do
     --readAddressWord 0x08000000 >>= liftIO . print
     liftIO $ clear [GLUT.ColorBuffer]
     record <- recordLCDControl
-    _backgrounds <- if bgMode record <= 2 then tileModes record else tileModes record
-    _sprites <- readOAM $ objCharacterVRAMMapping record
+    backgrounds <- if bgMode record <= 2 then tileModes record else bitmapModes record
+    sprites <- readOAM $ objCharacterVRAMMapping record
+    let screenObjs = sortBy comparePrio (sprites ++ backgrounds)
+    liftIO $ renderScreenObj screenObjs
     liftIO $ GLUT.swapBuffers
 
 animate :: TXChan SystemState -> GLUT.IdleCallback
