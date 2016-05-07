59c0627a8c9635e41dd0b7ef0c42148f918b07e1
Harry Duce <harry.duce@gmail.com>
2016-05-07 15:23:02 +0100

    Add function to map drawTile over Tile's within ScreenObj

--- src/Emulator/Video/Renderer.hs
+++ src/Emulator/Video/Renderer.hs
@@ -7,9 +7,20 @@ import Control.Monad.IO.Class
 import Data.Array.Storable
 import Graphics.Rendering.OpenGL
 
-drawTile :: StorableArray Address HalfWord -> QuadCoords -> IO ()
-drawTile arr ((x1, y1), (x2, y2), (x3, y3), (x4, y4)) = do
-  _ <- liftIO $ loadTexture arr
+renderScreenObj :: [ScreenObj] -> IO ()
+renderScreenObj [] = return ()
+renderScreenObj ((BG tiles _ _):xs) = do
+  _ <- mapM drawTile tiles
+  renderScreenObj xs
+renderScreenObj ((Sprite tiles _):xs) = do
+  _ <- mapM drawTile tiles
+  renderScreenObj xs
+renderScreenObj ((Hidden):xs) = renderScreenObj xs
+
+drawTile :: Tile -> IO ()
+drawTile (Tile pixData ((x1, y1), (x2, y2), (x3, y3), (x4, y4))) = do
+  arr <- liftIO $ newListArray (0, length pixData) pixData
+  obj <- liftIO $ loadTexture arr
   textureFilter Texture2D $= ((Nearest, Nothing), Nearest)
   renderPrimitive Quads $ do
     texCoord $ TexCoord2 0 (0 :: GLdouble)
@@ -20,8 +31,9 @@ drawTile arr ((x1, y1), (x2, y2), (x3, y3), (x4, y4)) = do
     vertex $ Vertex2 x4 (y4 :: GLdouble)
     texCoord $ TexCoord2 0 (1 :: GLdouble)
     vertex $ Vertex2 x3 (y3 :: GLdouble)
+  deleteObjectName obj
 
-loadTexture :: StorableArray Address HalfWord -> IO TextureObject
+loadTexture :: StorableArray Int HalfWord -> IO TextureObject
 loadTexture arr = withStorableArray arr $ \ptr -> do
     tile <- genObjectName
     textureBinding Texture2D $= Just tile
